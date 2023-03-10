327ef4288983fa9daf980e4c79ffd713927acbac
Joel Milligan <joelmilligan24@gmail.com>
2023-03-10 12:33:51 +0800

    M2L3 - Themes

---
+++ themes/colors.ts
@@ -0,0 +1,14 @@
+const white = '#ffffff';
+
+export default {
+  blankedOutBackground: '#18181888',
+  standardBackground: white,
+  cancelText: 'red',
+  title: 'grey',
+  veryLightGrey: '#DFDFDF',
+  lightGrey: '#AAAAAA',
+  purple: 'purple',
+  buttonRed: 'red',
+  buttonWhite: white,
+  bodyTextColor: 'black',
+};
---
+++ themes/images.ts
@@ -0,0 +1,4 @@
+export default {
+  arrowDown: require('../assets/ArrowDown/arrowDown.png'),
+  favIcon: require('../assets/favicon.png'),
+};
