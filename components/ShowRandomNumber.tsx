8671dee4600e723ae2cc4753c9b0a0a36ae8cd8b
Joel Milligan <joelmilligan24@gmail.com>
2023-03-10 15:55:20 +0800

    M3L6 - custom hook - useIsMounted

--- components/ShowRandomNumber.tsx
+++ components/ShowRandomNumber.tsx
@@ -1,37 +1,32 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Alert, Text } from 'react-native';
+
+import { useIsMounted } from '../utils/common-hooks';
 
 export function ShowRandomNumber() {
   const [result, setResult] = useState<number | undefined>();
 
-  const isMounted = useRef(false);
+  const isMounted = useIsMounted();
 
   useEffect(() => {
-    isMounted.current = true;
-    return () => {
-      isMounted.current = false;
-    };
-  }, []);
-
-  useEffect(() => {
-    if (result === undefined) {
+    if (result == null) {
       fetchRandomNumber()
         .then((res) => {
-          if (isMounted.current) setResult(res);
+          if (isMounted()) setResult(res);
           else Alert.alert("Didn't set state");
         })
         .catch((err) => {
-          if (isMounted.current) setResult(undefined);
+          if (isMounted()) setResult(undefined);
           console.warn(err);
         });
     }
-  }, [result]);
+  }, [isMounted, result]);
 
   const onPress = useCallback(() => {
     setResult(undefined);
   }, []);
 
-  if (result === undefined) return <Text>Waiting on a number</Text>;
+  if (result == null) return <Text>Waiting on a number</Text>;
 
   return <Text onPress={onPress}>{`Random number: ${result}`}</Text>;
 }
