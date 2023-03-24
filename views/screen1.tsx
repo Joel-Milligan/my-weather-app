733bf2ef1fb46c055e1b4df92333fdee9f46648d
Joel Milligan <joelmilligan24@gmail.com>
2023-03-24 13:46:29 +0800

    M6L2 - Redux actions

--- views/screen1.tsx
+++ views/screen1.tsx
@@ -1,14 +1,17 @@
 import React, { ReactElement } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { useDispatch } from 'react-redux';
 
 import { DefaultStackScreenProps } from './nav-types';
 import { SimpleButton } from '../components/SimpleButton';
+import { GlobalLoaderActions } from '../reducers/global-loader/reducer';
 import { colors } from '../theme/colors';
 import { metrics } from '../theme/metrics';
 
 type Props = DefaultStackScreenProps<'Screen1'>;
 
 export function Screen1(props: Props): ReactElement {
+  const dispatch = useDispatch();
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Screen 1 Body</Text>
@@ -18,6 +21,14 @@ export function Screen1(props: Props): ReactElement {
           props.navigation.navigate('Screen2');
         }}
         style={styles.button}
+      />
+      <SimpleButton
+        title="Load"
+        onPress={() => {
+          dispatch(GlobalLoaderActions.show({ message: 'Loading' }));
+        }}
+        style={styles.button}
+        secondary
       />
     </View>
   );
